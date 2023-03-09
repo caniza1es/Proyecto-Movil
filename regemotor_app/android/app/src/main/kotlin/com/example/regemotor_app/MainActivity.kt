df5f3f91fd0d4093685a5655738c59e18f4d57b3
happyterMWTH <62663250+happyterMWTH@users.noreply.github.com>
2023-03-09 14:58:07 -0500

    delete

--- regemotor_app/android/app/src/main/kotlin/com/example/regemotor_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.regemotor_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- regemotor_app/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'UI/my_app.dart';
-
-void main() {
-  runApp(const MyApp());
-}
